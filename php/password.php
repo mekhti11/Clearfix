0b2b2ec915edea169829a0b70928d84aacac6cbb
mekhti11 <mekhti11@gmail.com>
2018-09-28 16:22:31 +0300

    merge conflict

---
+++ php/password.php
@@ -0,0 +1,15 @@
+<!-- formData.append("action", "send");
+formData.append("password", this.pword); -->
+
+<?php
+
+    include 'db.php';
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json, charset: utf-8');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers , Content-Type , Access-Control-Allow-Methods , Authorization , X-Requested-With');
+
+    
+
+ ?>
